3822638d1a582c3c9965c48266d6948146e4520b
Kurimary Igor <suwigor@gmail.com>
2019-11-02 00:24:40 +0300

    Попробовал поделать дз

---
+++ lab3/hi.py
@@ -0,0 +1,14 @@
+from graph import *
+brushColor('yellow')
+circle(250,250,100)
+brushColor('red')
+circle(210,225,25)
+circle(290,225,25)
+
+
+brushColor('black')
+circle(210,225,10)
+circle(290,225,10)
+
+polygon([(235,250),(245,230),(100,100),(90,110),(235,250)])
+run()
