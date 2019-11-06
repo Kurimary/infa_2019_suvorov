b219994d298be2ec7c18bb0be379e641fb4fc4ca
Igor Notebook <suwigor@gmail.com>
2019-11-06 15:40:58 +0700

    Test file to Tkinter

---
+++ Test/Tets.py
@@ -0,0 +1,9 @@
+if __name__ == "__main__":
+    print('Launch as main file')
+    def support(x,y):
+        return x*y
+    print(support(2,7))
+else:
+    print("Imported library")
+    def Hello():
+        print('Hello!')
---
+++ Test/main.py
@@ -0,0 +1,6 @@
+import Tets as Te
+
+if __name__ == "__main__":
+
+    print("123")
+    Te.Hello()
